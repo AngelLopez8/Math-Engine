0feeb1c50eb60b30437a5388577f19ca60655c4c
Angel <angelomarlopez10@gmail.com>
2021-11-22 12:58:12 -0600

    Removed Exception Class

--- Main.cpp
+++ Main.cpp
@@ -19,15 +19,6 @@ int main() {
     AMathEngine::Vector c(tempC);
     AMathEngine::Vector d{tempD};
 
-    // AMathEngine::Vector3D v1{3.0f, 1.0f, -1.0f};
-    // AMathEngine::Vector3D v2{2.0f, 0.0f, 4.0f};
-    // AMathEngine::Vector3D v3{-1.0f, -2.0f, 3.0f};
-
-    // float mag = AMathEngine::magnitude(a);
-    // float dot = AMathEngine::dot(a, a);
-    // float angle = AMathEngine::find_angle(a, b);
-    // float scalarTripleProduct = AMathEngine::scalar_triple_product(v1, v2, v3);
-
     vector<AMathEngine::Vector> m1 {a, b, c, d, d};
 
     AMathEngine::Matrix A(m1);
--- Vector/include/Vector.h
+++ Vector/include/Vector.h
@@ -5,8 +5,6 @@
 #include <math.h>
 #include <vector>
 
-#include "Exception.h"
-
 # define M_PI   3.14159265358979323846  /* pi */
 
 using std::ostream;
